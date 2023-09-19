afe53a41463d3de8afac9b64299ad97ef09ae819
Deepesh2023 <officialdeepeshsg@gmail.com>
2023-09-19 16:40:09 +0530

    tweaked the grid

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+// creating the grid
+function createGrid() {
+  for (i = 0; i < 15; i++) {
+    var cloned_rowBox = rowBox.cloneNode(true);
+    container.appendChild(cloned_rowBox);
+    for (j = 0; j < 15; j++) {
+      var cloned_columnBox = columnBox.cloneNode(true);
+      cloned_rowBox.appendChild(cloned_columnBox);
+    }
+  }
+}
+
 const container = document.querySelector(".container");
 const rowBox = document.createElement("div");
 rowBox.classList.add("rowBox");
 const columnBox = document.createElement("div");
 columnBox.classList.add("columnBox");
 
-for (i = 0; i < 15; i++) {
-  var cloned_rowBox = rowBox.cloneNode(true);
-  container.appendChild(cloned_rowBox);
-  for (j = 0; j < 15; j++) {
-    var cloned_columnBox = columnBox.cloneNode(true);
-    cloned_rowBox.appendChild(cloned_columnBox);
-  }
-}
+createGrid();
+
+columnBox.addEventListener("click", () => {
+  console.log("poda patti");
+});
