6ca3d6f0ba1d31d6a27f4b3ada651ee1910f5d61
Deepesh2023 <officialdeepeshsg@gmail.com>
2023-09-20 16:39:14 +0530

    added buttons and text field

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 // creating the grid
-function createGrid() {
-  for (i = 0; i < 15; i++) {
+function createGrid(gridSize) {
+  for (i = 0; i < gridSize; i++) {
     var cloned_rowBox = rowBox.cloneNode(true);
     container.appendChild(cloned_rowBox);
-    for (j = 0; j < 15; j++) {
+    for (j = 0; j < gridSize; j++) {
       var cloned_columnBox = columnBox.cloneNode(true);
       cloned_rowBox.appendChild(cloned_columnBox);
     }
   }
 }
-
 const container = document.querySelector(".container");
 const rowBox = document.createElement("div");
 rowBox.classList.add("rowBox");
 const columnBox = document.createElement("div");
 columnBox.classList.add("columnBox");
 
-createGrid();
-
-columnBox.addEventListener("click", () => {
-  console.log("poda patti");
-});
+createGrid(+gridSize);
